5ce9c988320ba76cf16a895c9c62fc1f18f950ef
azzamjafri <azzamjafri98@gmail.com>
2019-08-30 20:26:46 +0530

    Login System Added

---
+++ src/main/java/com/hacksquare/events/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.hacksquare.events.repository;
+
+import com.hacksquare.events.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+
+}
